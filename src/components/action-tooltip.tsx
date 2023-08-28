86a44efbe35575ed623e5dc35796b985e4cceb6b
XNCOAISN <end.pgm@gmail.com>
2023-08-28 22:12:35 +0900

    ✨ add navigation bar

---
+++ src/components/action-tooltip.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { ComponentProps, ReactNode } from "react";
+
+type TooltipContentProps = ComponentProps<typeof TooltipContent>;
+
+type ActionTooltipProps = {
+  label: string;
+  children: ReactNode;
+  side?: TooltipContentProps["side"];
+  align?: TooltipContentProps["align"];
+};
+
+export const ActionTooltip = ({
+  label,
+  children,
+  side,
+  align,
+}: ActionTooltipProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={50}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent side={side} align={align}>
+          <p className="font-semibold text-sm capitalize">
+            {label.toLowerCase()}
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
