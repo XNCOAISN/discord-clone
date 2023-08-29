2294f7351e7ad2cb25a768ba178cc19ba46266c7
XNCOAISN <end.pgm@gmail.com>
2023-08-30 04:52:18 +0900

    ✨ leave server

--- src/components/server/server-header.tsx
+++ src/components/server/server-header.tsx
@@ -85,7 +85,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {!isAdmin && (
-          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("leaveServer", { server })}
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+          >
             Leave Server
             <LogOut className="h-5 w-5 ml-auto" />
           </DropdownMenuItem>
